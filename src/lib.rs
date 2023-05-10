f4fe71263b46aa9aa3dc0524f162f54b424393b4
Alex Snaps <alex@wcgw.dev>
2023-05-09 22:38:22 -0400

    Adding base API to some Db struct for now (#11)
    * Adding base API to some Db struct for now
    
    * Fix clippy

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,68 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-pub mod memtable;
+use crate::memtable::{Memtable, MemtableResult};
+
+pub(crate) mod memtable;
+
+#[derive(Default)]
+pub struct Db<'a> {
+  mem: Memtable<'a>,
+  imm: Option<Memtable<'a>>,
+  // disk: ,
+}
+
+impl<'a> Db<'a> {
+  pub fn get<K>(&'a self, key: K) -> Option<Vec<u8>>
+  where
+    K: AsRef<[u8]>,
+  {
+    let key = key.as_ref();
+
+    match self.mem.get(key) {
+      MemtableResult::Hit(hit) => hit,
+      MemtableResult::Miss => {
+        if let Some(imm) = self.imm.as_ref() {
+          if let MemtableResult::Hit(hit) = imm.get(key) {
+            return hit;
+          }
+        }
+        // return self.disk
+        None
+      }
+    }
+  }
+
+  pub fn put<K, V>(&'a self, key: K, value: V)
+  where
+    K: AsRef<[u8]>,
+    V: AsRef<[u8]>,
+  {
+    let key = key.as_ref();
+    let val = value.as_ref();
+    self.mem.add(key, val);
+  }
+
+  pub fn delete<K>(&'a self, key: K)
+  where
+    K: AsRef<[u8]>,
+  {
+    let key = key.as_ref();
+    self.mem.delete(key);
+  }
+}
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+  use crate::Db;
+
+  #[test]
+  fn it_works() {
+    let db = Db::default();
+    assert_eq!(db.get(b"42"), None);
+    db.put(b"42", b"An answer to some question");
+    assert_eq!(db.get(b"42"), Some(b"An answer to some question".to_vec()));
+    db.delete(b"42");
+    assert_eq!(db.get(b"42"), None);
+  }
+}
